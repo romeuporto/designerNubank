b0d4542a2fb334ea9870d52c586d3956b4cb0da1
romeuporto <romeu.porto.sms@gmail.com>
2022-07-04 22:36:30 -0300

    dados da conta

--- designer_nubank/lib/widgets/menu_app.dart
+++ designer_nubank/lib/widgets/menu_app.dart
@@ -18,8 +18,18 @@ class MenuApp extends StatelessWidget {
           duration: Duration(milliseconds: 200),
           opacity: showMenu ? 1 : 0,
           child: Container(
-            color: Colors.red,
+            color: Colors.purple.shade800,
             height: MediaQuery.of(context).size.height * 0.55,
+            child: Column(
+              children: [
+                Image.network('https://cdn-0.imagensemoldes.com.br/wp-content/uploads/2020/08/QR-Code-PNG-1024x1024.png',
+                  height: 80,
+                  color: Colors.white,
+
+
+                ),
+              ],
+            ),
           ),
         ));
   }
